8999db7ed1503d4b6eb88d2cd1723c0a1b3ef519
fonstok <jnk@fonstok.com>
2016-03-05 13:17:29 -0600

    ran hint

--- jquery.jfLightBoxLoad.js
+++ jquery.jfLightBoxLoad.js
@@ -2,9 +2,10 @@
 
     $.jfLightBoxLoad = function(element, options) {
         var plugin = this;
-        var $element = $(element),
-             element = element;
+        var $element = $(element);
         var $ldElement;
+        var $lb;
+        var $lbWin;
         var dataatts = $element.data();
         var win = false;
 
@@ -26,7 +27,7 @@
             onCompleteArgs: [],
             onClosed: function() {}, 
             onClosedArgs: [],
-        }
+        };
         plugin.settings = {};
 
         plugin.init = function() {
@@ -36,59 +37,14 @@
             if (plugin.settings.useTransit){
                 $.fn.animate = $.fn.transition;
             }	
-			if (plugin.settings.event != 'none'){
-				$element.bind(plugin.settings.mouseEvent, onMouse)
+			if (plugin.settings.event !== 'none'){
+				$element.bind(plugin.settings.mouseEvent, onMouse);
 			}
-        }
+        };
         // mouse event
         function onMouse(event){
             event.preventDefault();              
             plugin.launch();
-        };
-        // launch function
-        plugin.launch = function(){
-            win = true;
-            // call on start function
-            plugin.settings.onStart.apply(plugin,plugin.settings.onStartArgs);
-            // make tags
-            var tag = new Array();
-            tag.push('<div class="lb_lightbox">');
-            tag.push('<div class="lb_shade"></div>');
-            tag.push('<div class="lb_loadAnimation">loading</div>');
-            tag.push('<div class="lb_window">');
-            // iframe ?
-            if (plugin.settings.iframe){
-                tag.push('<iframe class="lb_content" frameborder="0" src ='+plugin.settings.hash+' width="100%" height:"100%"></iframe>');
-            } else {
-                tag.push('<div class="lb_content"></div>');
-            }
-            tag.push('<div class="lb_closeBtn">close</div>');
-            tag.push('</div></div>');
-            var tagString = '';
-            $(tag).each(function(index, element){
-                tagString+=element;
-            });
-            $ldElement.prepend(tagString);
-            // cache objects 
-            $lb = $('.lb_lightbox');  
-            $lbWin = $('.lb_window');
-            // load into content or iframe ?
-            if (plugin.settings.iframe){
-                $('.lb_content').load(loadComplete);
-            } else {
-               $('.lb_content').load(plugin.settings.hash, loadComplete); 
-            }
-            // shade animation
-            $('.lb_shade').css({opacity:'0'})
-                .animate({opacity:'1'}, plugin.settings.speed);
-            // set, load, and animate window
-            $lbWin.css(plugin.settings.animationFrom);
-            function loadComplete() {
-                $lbWin.delay(plugin.settings.pause).animate(plugin.settings.animationTo, plugin.settings.speed, plugin.settings.ease, completed);
-                $('.lb_loadAnimation').animate({opacity:'0'}, plugin.settings.speed*.8, function(){
-                    $(this).remove();
-                });     
-            };          
         }
         function completed(){
             // add close functionality
@@ -109,24 +65,71 @@
                 plugin.settings.onClosed.apply(plugin,plugin.settings.onClosedArgs);
                 win = false;
             });  
-        }  
+        };
         plugin.destroy = function(){
             if (win){
                 plugin.close();
-            }  
-            $element.unbind(plugin.settings.mouseEvent, onMouse)
+            }
+            $element.unbind(plugin.settings.mouseEvent, onMouse);
             $element.removeData('jfLightBoxLoad', plugin);
             plugin = null;
-        }  
+        };
+       
+        plugin.launch = function(){
+            //------------------------ launch function
+            win = true;
+            // call on start function
+            plugin.settings.onStart.apply(plugin,plugin.settings.onStartArgs);
+            // make tags
+            var tag = [];
+            tag.push('<div class="lb_lightbox">');
+            tag.push('<div class="lb_shade"></div>');
+            tag.push('<div class="lb_loadAnimation">loading</div>');
+            tag.push('<div class="lb_window">');
+            // iframe ?
+            if (plugin.settings.iframe){
+                tag.push('<iframe class="lb_content" frameborder="0" src ='+plugin.settings.hash+' width="100%" height:"100%"></iframe>');
+            } else {
+                tag.push('<div class="lb_content"></div>');
+            }
+            tag.push('<div class="lb_closeBtn">close</div>');
+            tag.push('</div></div>');
+            var tagString = '';
+            $(tag).each(function(index, element){
+                tagString+=element;
+            });
+            $ldElement.prepend(tagString);
+            // cache objects 
+            $lb = $('.lb_lightbox');  
+            $lbWin = $('.lb_window');
+            
+            function loadComplete() {
+                $lbWin.delay(plugin.settings.pause).animate(plugin.settings.animationTo, plugin.settings.speed, plugin.settings.ease, completed);
+                $('.lb_loadAnimation').animate({opacity:'0'}, plugin.settings.speed*0.8, function(){
+                    $(this).remove();
+                });     
+            }
+            // load into content or iframe ?
+            if (plugin.settings.iframe){
+                $('.lb_content').load(loadComplete);
+            } else {
+               $('.lb_content').load(plugin.settings.hash, loadComplete); 
+            }
+            // shade animation
+            $('.lb_shade').css({opacity:'0'})
+                .animate({opacity:'1'}, plugin.settings.speed);
+            // set, load, and animate window
+            $lbWin.css(plugin.settings.animationFrom);         
+        };  
         plugin.init();
-    }
+    };
 
     $.fn.jfLightBoxLoad = function(options) {
         return this.each(function() {
-            if (undefined == $(this).data('jfLightBoxLoad')) {
+            if (undefined === $(this).data('jfLightBoxLoad')) {
                 var plugin = new $.jfLightBoxLoad(this, options);
                 $(this).data('jfLightBoxLoad', plugin);
             }
         });
-    }
+    };
 })(jQuery);
